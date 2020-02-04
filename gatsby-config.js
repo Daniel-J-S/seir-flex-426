15b5bd0686cd2fcb8f9972905076cd1741a49eea
myDeveloperJourney <daniel@mydeveloperjourney.com>
2020-02-03 21:18:35 -0600

    adds lastest changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  siteMetadata: {
+    title: 'GA Student Website',
+    author: 'DanielJS',
+    currentYear: new Date.getFullYear(),
+    description: 'An interactive website for GA Students by Instructors'
+  },
   plugins: [
     'gatsby-plugin-sass',
     'gatsby-plugin-react-helmet'
