fce46e7f028d7914dd563119abacd6700b634c56
myDeveloperJourney <daniel@mydeveloperjourney.com>
2020-02-04 22:55:46 -0600

    adds additional link

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -12,7 +12,10 @@ export default () => (
                 <Link to="/">Home</Link>
             </li>
             <li>
-                <a href="https://forms.gle/ZhsdJF2JpgvAK41h6">Submit Homework</a>
+                <Link to="/course-details">Course Details</Link>
+            </li>
+            <li>
+                <a href="https://forms.gle/ZhsdJF2JpgvAK41h6" target="_blank" rel="noopener noreferrer">Submit Homework</a>
             </li>
         </ul>
     </nav>
