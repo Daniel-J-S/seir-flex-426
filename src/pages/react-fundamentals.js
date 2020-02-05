56239236f0e557b33b22284549ad598db593c7fa
myDeveloperJourney <daniel@mydeveloperjourney.com>
2020-02-04 22:53:46 -0600

    sets up homepage for react fundamentals

---
+++ src/pages/react-fundamentals.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+
+import Layout from '../components/layout';
+
+export default ({ data }) => {
+    const { allMarkdownRemark } = data;
+
+    const html = allMarkdownRemark.edges[0].node.html;
+    return (
+        <Layout pageTitle={"Home"}>
+            <main dangerouslySetInnerHTML={{__html: html}} />
+        </Layout>
+    );
+
+}
+
+export const query = graphql`
+    query {
+        allMarkdownRemark (
+        filter: { fileAbsolutePath: {regex : "\/react-fundamentals/"} }
+    ) {
+        edges {
+                node {
+                    frontmatter {
+                    title
+                    date
+                    }
+                    html
+                }
+            }
+        }
+    }
+`;
