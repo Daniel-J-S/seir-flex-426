0868f22f8428ce206921b058116cb8032accdd25
myDeveloperJourney <daniel@mydeveloperjourney.com>
2020-02-18 23:43:16 -0600

    adds addiitonal content

--- src/pages/react-fundamentals.js
+++ src/pages/react-fundamentals.js
@@ -1,34 +1,53 @@
 import React from 'react';
-import { graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 
 import Layout from '../components/layout';
 
-export default ({ data }) => {
-    const { allMarkdownRemark } = data;
 
-    const html = allMarkdownRemark.edges[0].node.html;
+export default({ data }) => {
+
+   const { allMarkdownRemark } = data;
+
+   const lessons = allMarkdownRemark.edges.map(({ node }) => 
+      <section key={node.id}>
+          <Link to={node.fields.slug}>
+              <h2>{node.frontmatter.title}</h2>
+          </Link>
+              <p>{node.frontmatter.topics}</p>
+      </section>
+   );
+
     return (
-        <Layout pageTitle={"Home"}>
-            <main dangerouslySetInnerHTML={{__html: html}} />
+        <Layout pageTitle="React Fundamentals">
+            <h1>React Fundamentals</h1>
+            {lessons}
         </Layout>
     );
-
 }
 
 export const query = graphql`
-    query {
-        allMarkdownRemark (
-        filter: { fileAbsolutePath: {regex : "\/react-fundamentals/"} }
-    ) {
-        edges {
-                node {
-                    frontmatter {
-                    title
-                    date
-                    }
-                    html
-                }
-            }
+query {
+    allMarkdownRemark (
+          filter: {frontmatter: {track: {eq: "React Fundamentals"}, 
+          type: {eq: "homepage"}}}
+            sort: {fields: frontmatter___day }
+        ){
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            track
+            title
+            week
+            day
+            type
+            topics
+            slug
+          }
         }
+      }
     }
+  }
 `;
