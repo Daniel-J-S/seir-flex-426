69fd1c84ae30032a35f7c4bd987dba2659fc4aab
myDeveloperJourney <daniel@mydeveloperjourney.com>
2020-02-21 16:41:13 -0600

    further builds out lecture card

--- src/components/lecture-card/index.js
+++ src/components/lecture-card/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'gatsby';
+
+import styles from './lecture-card.module.scss';
 
 export default (props) => {
     return (
-        <article>
-
-        </article>
+    <Link to={props.slug}>
+        <section className={styles.card}>
+            <h2>{props.title}</h2>
+            <p>{props.topics}</p>
+        </section>
+    </Link>
     );
 }
