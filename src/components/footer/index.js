4ec3071e3ff58ff94fccbea3db36e1b82df135fe
myDeveloperJourney <daniel@mydeveloperjourney.com>
2020-02-21 17:01:44 -0600

    adds social media icons

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 import styles from './footer.module.scss';
 
 export default () => (
-    <footer className={styles.footer}></footer>
+    <footer className={styles.footer}>
+        <p>Copyright &copy; General Assembly {new Date().getFullYear()}</p>
+        <section>
+            <a href="https://www.facebook.com/generalassembly/" target="_blank">
+                <FontAwesomeIcon icon={faFacebook} size="lg" rel="noopener noreferrer"/>
+            </a>
+            <a href="https://www.linkedin.com/school/general-assemb-ly/" target="_blank">
+                <FontAwesomeIcon icon={faLinkedin} size="lg" rel="noopener noreferrer"/>
+            </a>
+            <a href="https://twitter.com/GA" target="_blank">
+                <FontAwesomeIcon icon={faTwitter} size="lg" rel="noopener noreferrer"/>
+            </a>
+        </section>
+    </footer>
 );
