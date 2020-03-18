02dab57004abd52560ffad24f8ff08fa9b505b26
myDeveloperJourney <daniel@mydeveloperjourney.com>
2020-03-18 13:05:30 -0500

    adds additional unit homepages

---
+++ src/pages/second-language.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+
+import Layout from '../components/layout';
+import LectureCard from '../components/lecture-card';
+
+import styles from '../styles/course-content-display.module.scss';
+
+
+export default({ data, location }) => {
+    
+   const { allMarkdownRemark } = data;
+
+   const lessons = allMarkdownRemark.edges.map(({ node }) => 
+      <LectureCard 
+        key={node.id}
+        slug={node.fields.slug}
+        title={node.frontmatter.title}
+        topics={node.frontmatter.topics} 
+      />
+   );
+
+    return (
+        <Layout 
+          pageTitle="Second Language" 
+          location={location} 
+          crumbLabel={"Second Language"}>
+            <h1>Second Language</h1>
+            <main className={styles.main}>
+              {lessons}
+            </main>
+        </Layout>
+    );
+}
+
+export const query = graphql`
+query {
+    allMarkdownRemark (
+          filter: {frontmatter: {track: {eq: "Second Language"}, 
+          type: {eq: "homepage"}}}
+            sort: {fields:  [frontmatter___week, frontmatter___day]}
+        ){
+      edges {
+        node {
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            track
+            title
+            week
+            day
+            type
+            topics
+          }
+        }
+      }
+    }
+  }
+`;
